a0f9c2d354f2bb4da74d2893807cf769fe105976
Binita Gupta <binitagupta24@gmail.com>
2022-09-14 00:22:06 -0400

    Initial Commit

---
+++ Shared/todo_flutter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_flutter_appApp.swift
+//  Shared
+//
+//  Created by Binita Gupta on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct todo_flutter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
